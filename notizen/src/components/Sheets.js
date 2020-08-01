252061bf6b780627f69a5cd3598dbb2c2635bbb5
Valencia <davidalencia@gmail.com>
2020-07-31 19:06:08 -0500

    strange pixi result

--- notizen/src/components/Sheets.js
+++ notizen/src/components/Sheets.js
@@ -1,29 +1,107 @@
 import React, { useEffect, useRef } from 'react';
 import * as PIXI from 'pixi.js'
 
+/**
+ * Cubic interpolation based on https://github.com/osuushi/Smooth.js
+ */
+function clipInput(k, arr) {
+  if (k < 0) k = 0;
+  if (k > arr.length - 1) k = arr.length - 1;
+  return arr[k];
+}
+
+function getTangent(k, factor, array) {
+  return factor * (clipInput(k + 1, array) - clipInput(k - 1, array)) / 2;
+}
+
+function cubicInterpolation(array, t, tangentFactor) {
+  if (tangentFactor == null) tangentFactor = 1;
+
+  const k = Math.floor(t);
+  const m = [getTangent(k, tangentFactor, array), getTangent(k + 1, tangentFactor, array)];
+  const p = [clipInput(k, array), clipInput(k + 1, array)];
+  t -= k;
+  const t2 = t * t;
+  const t3 = t * t2;
+  return (2 * t3 - 3 * t2 + 1) * p[0] + (t3 - 2 * t2 + t) * m[0] + (-2 * t3 + 3 * t2) * p[1] + (t3 - t2) * m[1];
+}
+
+
+
+
+
+
+
+
+
 const Sheets = () => {  
   const divRef = useRef();
   
   useEffect(()=>{
-    console.log(divRef.current);
-    const app = new PIXI.Application({ transparent: true });
-    document.body.appendChild(app.view);
+    const box = divRef.current.getBoundingClientRect();
+    const devicePixelRatio = 1 //window.devicePixelRatio || 1;
+    const app = new PIXI.Application({ 
+      width: (box.width) / devicePixelRatio, 
+      height: (window.innerHeight -box.y -14) / devicePixelRatio, 
+      transparent: true,
+      resolution: devicePixelRatio
+    });
+    divRef.current.appendChild(app.view);
+ 
+
+
+
+    ///--------------------PEN----------------------------------
+    const trailTexture = PIXI.Texture.from('images/eraser.svg');
+    const historyX = [];
+    const historyY = [];
+    // historySize determines how long the trail will be.
+    const historySize = 50;
+    // ropeSize determines how smooth the trail will be.
+    const ropeSize = 100;
+    const points = [];
     
-    // create a new Sprite from an image path.
-    const bunny = PIXI.Sprite.from('./images/eraser.svg');
+    // Create history array.
+    for (let i = 0; i < historySize; i++) {
+        historyX.push(0);
+        historyY.push(0);
+    }
+    // Create rope points.
+    for (let i = 0; i < ropeSize; i++) {
+        points.push(new PIXI.Point(0, 0));
+    }
     
-    // center the sprite's anchor point
-    bunny.anchor.set(0.5);
+    // Create the rope
+    const rope = new PIXI.SimpleRope(trailTexture, points);
     
-    // move the sprite to the center of the screen
-    bunny.x = app.screen.width / 2;
-    bunny.y = app.screen.height / 2;
+    // Set the blendmode
+    rope.blendmode = PIXI.BLEND_MODES.ADD;
     
-    app.stage.addChild(bunny);
+    app.stage.addChild(rope);
+
     
-    app.ticker.add(() => {
-        // just for fun, let's rotate mr rabbit a little
-        bunny.rotation += 0.1;
+    // Listen for animate update
+    app.ticker.add((delta) => {
+        // Read mouse points, this could be done also in mousemove/touchmove update. For simplicity it is done here for now.
+        // When implementing this properly, make sure to implement touchmove as interaction plugins mouse might not update on certain devices.
+        const mouseposition = app.renderer.plugins.interaction.mouse.global;
+    
+        // Update the mouse values to history
+        historyX.pop();
+        historyX.unshift(mouseposition.x);
+        historyY.pop();
+        historyY.unshift(mouseposition.y);
+        // Update the points to correspond with history.
+        for (let i = 0; i < ropeSize; i++) {
+            const p = points[i];
+    
+            // Smooth the curve with cubic interpolation to prevent sharp edges.
+            const ix = cubicInterpolation(historyX, i / ropeSize * historySize);
+            const iy = cubicInterpolation(historyY, i / ropeSize * historySize);
+    
+            p.x = ix;
+            p.y = iy;
+        }
     });
   }, [])
 
